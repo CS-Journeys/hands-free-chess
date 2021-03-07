61668692a1cdb994cef16d26c707be1734c821e4
daniel-luper <jdaniel.luper@gmail.com>
2021-03-07 14:12:45 -0600

    UI pausing and bug fixes

--- src/user_interface.py
+++ src/user_interface.py
@@ -19,109 +19,120 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 import os
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
+from functools import partial
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 import logging
 
-# ChessUI 
+
+# ChessUI
 # Description: UI class for the hands free application
 # Extends the QWidget module
 # Parameters: None
 class ChessUI(QWidget):
     def __init__(self):
-        super().__init__() #Avoids reference to base class explicitly
-        self.layout = QVBoxLayout() #Vertically aligned window object
-        self.log = logging.getLogger(__name__) #Application log object
-        self.scroll = QScrollArea() #Scroll bar object
-        self.scrollWidget = QWidget() #Scroll widget object
+        super().__init__()  # Avoids reference to base class explicitly
+        self.layout = QVBoxLayout()  # Vertically aligned window object
+        self.log = logging.getLogger(__name__)  # Application log object
+        self.scroll = QScrollArea()  # Scroll bar object
+        self.scrollWidget = QWidget()  # Scroll widget object
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.scrollWidget.setWindowFlags(Qt.WindowStaysOnTopHint)
 
-        #UI Buttons
+        # Make scroll bar lock to bottom
+        self.scroll_bar = self.scroll.verticalScrollBar()
+        self.scroll_bar.rangeChanged.connect(lambda: self.scroll_bar.setValue(self.scroll_bar.maximum()))
+
+        # UI Buttons
         self.start_button = QPushButton("Start")
-        self.pause_button = QPushButton("Pause (coming soon)")
+        self.pause_button = QPushButton("Pause")
         self.exit_button = QPushButton("Exit")
         self.help = QPushButton("Help")
 
-        self.thread = None #Used for pause functionality
+        self.thread = None # The thread that will run once 'start' is pressed
 
         self.paused = False
-        self.init_ui() #Instantiate UI
+        self.init_ui()  # Instantiate UI
 
     def customEvent(self, event):
         if event.type() == 100:
             self.print_to_user(event.data())
-    
+
     # init_ui
     # Description: Instantiates all UI widgets
     # Parameters: None
     # Return: Void
     def init_ui(self):
         self.log.info("Initializing UI")
-        self.start_button.clicked.connect(self.button_clicked) #Call click function when start is clicked
+        self.start_button.clicked.connect(self.button_clicked)  # Call click function when start is clicked
         self.pause_button.setEnabled(False)
-        # self.pause_button.clicked.connect(self.button_clicked)
+        self.pause_button.clicked.connect(self.button_clicked)
         self.exit_button.clicked.connect(self.button_clicked)
         self.help.clicked.connect(self.open_help)
 
-        self.scrollWidget.setLayout(QVBoxLayout()) #Set scroll alignment to vertical
-        self.scroll.setWidget(self.scrollWidget) #Set scroll widget to the scroll widget object
+        self.scrollWidget.setLayout(QVBoxLayout())  # Set scroll alignment to vertical
+        self.scroll.setWidget(self.scrollWidget)  # Set scroll widget to the scroll widget object
 
-        temp = QLabel('Click \"Start\" to begin.') #Click start label
-        temp.setFixedHeight(20) #Set height of label
-        self.scrollWidget.layout().addWidget(temp) #Add click start message to scroll window frame
-        self.scrollWidget.layout().setAlignment(Qt.AlignTop) #Allign scroll window frame
-        self.scroll.setWidgetResizable(True) #Allow resizable scroll frame
-        self.scroll.setFixedHeight(400) #Set default scroll frame height
-        self.scroll.setFixedWidth(400) #Set default scroll frame width
+        temp = QLabel('Click \"Start\" to begin.')  # Click start label
+        temp.setFixedHeight(20)  # Set height of label
+        self.scrollWidget.layout().addWidget(temp)  # Add click start message to scroll window frame
+        self.scrollWidget.layout().setAlignment(Qt.AlignTop)  # Align scroll window frame
+        self.scroll.setWidgetResizable(True)  # Allow resizable scroll frame
+        self.scroll.setFixedHeight(400)  # Set default scroll frame height
+        self.scroll.setFixedWidth(400)  # Set default scroll frame width
 
-        #Add widgets to layout object
+        # Add widgets to layout object
         self.layout.addWidget(self.scroll)
-        self.layout.addWidget(self.start_button) 
+        self.layout.addWidget(self.start_button)
         self.layout.addWidget(self.pause_button)
         self.layout.addWidget(self.exit_button)
         self.layout.addWidget(self.help)
 
-        self.setWindowTitle("Hands Free Chess") #Set window title
-        self.setWindowIcon(QIcon('res/logo.png')) #Set window icon
-        self.setLayout(self.layout) #Set layout to layout object
+        self.setWindowTitle("Hands Free Chess")  # Set window title
+        self.setWindowIcon(QIcon('res/logo.png'))  # Set window icon
+        self.setLayout(self.layout)  # Set layout to layout object
 
-        self.show() #Display initialized UI
+        self.show()  # Display initialized UI
 
     # button_clicked
     # Description: Button click function
     # Parameters: Valid button call
     # Return: None
     def button_clicked(self):
-        sender = self.sender() #Get current button
-        if sender.text() == 'Start': #Start button execution
+        sender = self.sender()  # Get current button
+        if sender.text() == 'Start':  # Start button execution
             self.log.info("Start button pressed")
             if self.paused:
-                self.print_to_user("Restarting application...")
-                self.print_to_user("Please wait...")
-                self.log.info("Restarting app")
+                self.print_to_user("Resuming application...")
+                self.log.info("Resuming app")
+                self.thread.paused = False
             else:
                 self.print_to_user("Starting application...")
                 self.log.info("First app startup")
                 self.log.info("Starting thread")
                 self.thread.start()
+                self.thread.finished.connect(self.quit_app)
+                self.thread.pause.connect(self.pause)
+                self.thread.help.connect(self.open_help)
+                self.thread.paused = False
             self.paused = False
             self.start_button.setEnabled(False)
-            # self.pause_button.setEnabled(True)
+            self.pause_button.setEnabled(True)
 
-        #Pause button implementation work in progress
+        elif sender.text() == 'Pause':
+            self.log.info("Pause button pressed")
+            self.paused = True
+            self.thread.paused = True
+            self.start_button.setEnabled(True)
+            self.pause_button.setEnabled(False)
+            self.print_to_user("Application Paused")
 
-        # elif sender.text() == 'Pause':
-        #     self.log.info("Pause button pressed")
-        #     self.start_button.setEnabled(not self.paused)
-        #     self.pause_button.setEnabled(self.paused)
-        #     self.paused = True
-        #     self.thread.stop()
-        #     self.print_to_user("Application Paused")
-
-        #Exit button execution
+        # Exit button execution
         elif sender.text() == 'Exit':
             self.log.info("Exit button pressed")
-            self.thread.stop()
+            if self.thread.running:
+                self.thread.stop()
             self.quit_app()
 
     # quit_app
@@ -130,11 +141,16 @@ class ChessUI(QWidget):
     # Return: Application termination
     def quit_app(self):
         self.log.info("Quitting app")
-        self.start_button.setEnabled(False) #Disable start button
-        # self.pause_button.setEnabled(False)
         self.print_to_user("Bye...")
-        self.close() #Close the UI
+        self.close()  # Close the UI
         exit(0)
+
+    def pause(self):
+        self.print_to_user("Application Paused")
+        self.paused = True
+        self.thread.paused = True
+        self.start_button.setEnabled(True)
+        self.pause_button.setEnabled(False)
 
     # print_to_user
     # Description: Print log messages to scroll frame
@@ -142,10 +158,12 @@ class ChessUI(QWidget):
     # Return: Displayed message to user
     def print_to_user(self, msg):
         self.log.debug(f"Print: {msg}")
-        temp = QLabel(msg) #Message label UI element
+        temp = QLabel(msg)  # Message label UI element
         temp.setFixedHeight(20)
-        self.scrollWidget.layout().addWidget(temp) #Add element to window frame
-        self.scrollWidget.update() #Update scroll window frame
+        self.scrollWidget.layout().addWidget(temp)  # Add element to window frame
+        self.scrollWidget.update()  # Update scroll window frame
+        QTimer.singleShot(0, partial(self.scroll.ensureWidgetVisible, temp))
+        self.show()
 
     # open_help
     # Description: Help button functionality
